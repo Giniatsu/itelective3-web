621415ab0104fc0a0fd8f70e001646beef06cf79
Giniatsu <110588543+Giniatsu@users.noreply.github.com>
2022-10-09 19:30:18 +0800

    Poke API completed

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 var weather = require('weather-js');
 const dayjs = require('dayjs')
+var fetchUrl = require("fetch").fetchUrl;
 
 app.set('view engine', 'ejs');
 
-app.listen(8000)
+app.listen(3000)
 
 app.get('/', function (req, res) {
     weather.find({search: 'Davao City, PH', degreeType: 'C'}, function(err, result) {
@@ -15,6 +16,24 @@ app.get('/', function (req, res) {
         console.log(JSON.stringify(result, null, 2));
         weather_data = result;
        }
-       res.render('index', {panahon: weather_data, dayjs});
+       res.render('index', {title: 'Weather',panahon: weather_data, dayjs});
       });
+});
+
+app.get('/other', async function (req, res) {
+    if (req.query.pokemoninput) {
+        fetchUrl(`https://pokeapi.co/api/v2/pokemon/${req.query.pokemoninput}`, function(error, meta, body){
+            var meals = JSON.parse(body)
+            var data = {
+                itemData: meals
+            }
+            console.log(data.itemData)
+            res.render('other', {title:'PokeAPI', data});
+        });
+    } else {
+        var data = {
+            itemData: null
+        }
+        res.render('other', {title:'PokeAPI', data});
+    }
 });
